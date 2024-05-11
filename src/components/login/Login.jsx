69c85af0f83420a60dd9ec451bebd9fe8c75b88a
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-05-11 22:59:18 +0530

    Added Signup With Email and Password functionality

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 import './login.css';
+import { toast } from 'react-toastify';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../../firebase/firebase';
+import { doc, setDoc } from 'firebase/firestore';
+import upload from '../../firebase/upload';
 // import { toast } from 'react-toastify';
 
 const Login = () => {
@@ -7,6 +12,8 @@ const Login = () => {
         file: null,
         url: ""
     });
+
+    const [loading, setLoading] = useState(false);
 
     const handleAvatar = e => {
         if (e.target.files[0]) {
@@ -17,25 +24,71 @@ const Login = () => {
         }
     };
 
-    const handleLogin = (e) => {
+    //    For signup
+    const handleRegister = async (e) => {
         e.preventDefault();
-        // toast.success("Login success");
+        setLoading(true);
+        const formData = new FormData(e.target);
+        const { username, email, password } = Object.fromEntries(formData);
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+
+            const imgURL = await upload(avatar.file);
+
+            await setDoc(doc(db, "users", res.user.uid), {
+                username,
+                email,
+                avatar: imgURL,
+                id: res.user.uid,
+                blocked: []
+            });
+
+            await setDoc(doc(db, "userChats", res.user.uid), {
+                chats: []
+            });
+
+            toast.success("Account created🙌🏻. You can login now");
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    // for login
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const formData = new FormData(e.target);
+        const { email, password } = Object.fromEntries(formData);
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
         <div className='login'>
             <div className="item">
                 <h2>Welcome back</h2>
-                <form action="" onSubmit={handleLogin}>
+                <form onSubmit={handleLogin}>
                     <input type="email" placeholder='Email' name='email' />
                     <input type="password" placeholder='Password' name='password' />
-                    <button>Sign in</button>
+                    <button disabled={loading}>{loading ? "Loading" : "Sign in"}</button>
                 </form>
             </div>
             <div className="separator"></div>
             <div className="item">
                 <h2>Create an Account</h2>
-                <form action="">
+                <form onSubmit={handleRegister}>
                     <label htmlFor="file">
                         <img src={avatar.url || './avatar.png'} alt="" />
                         Upload an Image</label>
@@ -43,7 +96,7 @@ const Login = () => {
                     <input type="text" placeholder='Username' name='username' />
                     <input type="email" placeholder='Email' name='email' />
                     <input type="password" placeholder='Password' name='password' />
-                    <button>Sign up</button>
+                    <button disabled={loading}>{loading ? "Loading" : "Sign up"}</button>
                 </form>
             </div>
         </div>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
@@ -11,3 +14,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, auth, db, storage };
